ef48ea075cec09e23f2f40cbb1e95d44d34ba963
Luca Pipolo <pipolo.luca@gmail.com>
2026-08-07 10:57:44 +0200

    build: setup Astro Icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
+import icon from "astro-icon"
 
 import { DEFAULT_LOCALE, LOCALES } from "./src/libs/i18n.ts"
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   i18n: {
     locales: [...LOCALES],
     defaultLocale: DEFAULT_LOCALE,
